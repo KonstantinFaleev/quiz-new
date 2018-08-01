67699d4a734856b3efd616eaace638034ccea564
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-08-01 10:38:40 +0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuizNew::Application.config.secret_token = '08546f7bba769c7c9959572ed2a069ba5f4e33adc9c37d534717afd8d78ae511193f1462960a59fa33af9968351937adcd3467a0dc19d708f760dd8ee7dd2b1c'
